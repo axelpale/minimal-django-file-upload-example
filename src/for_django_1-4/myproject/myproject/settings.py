9ab39c7fe5abaf00f2d586e88e79a5a85075179e
gentoo90 <gentoo90@gmail.com>
2013-02-11 20:22:39 +0200

    Fix files permissions
    For existing files and files which will be uploaded in the future

--- src/for_django_1-4/myproject/myproject/settings.py
+++ src/for_django_1-4/myproject/myproject/settings.py
@@ -67,6 +67,8 @@ MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = '/media/'
 
+FILE_UPLOAD_PERMISSIONS = 0644
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
