16bac14dc8d1c0d0a73b13ae3cba4439a31b27f1
Yongfeng LI <wliyongfeng@gmail.com>
2015-11-18 14:13:09 +0800

    fix the expect 2 lines style problem

--- src/for_django_1-8/myproject/myproject/myapp/forms.py
+++ src/for_django_1-8/myproject/myproject/myapp/forms.py
@@ -2,6 +2,7 @@
 
 from django import forms
 
+
 class DocumentForm(forms.Form):
     docfile = forms.FileField(
         label='Select a file'
--- src/for_django_1-8/myproject/myproject/myapp/models.py
+++ src/for_django_1-8/myproject/myproject/myapp/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
+
 class Document(models.Model):
     docfile = models.FileField(upload_to='documents/%Y/%m/%d')
