e156a1342cdf5af2763619dceb00e6ee81f07d10
Kim Do Hyun <nero.union12@gmail.com>
2015-12-02 12:13:10 +0900

    Cleanup style pep8 only django 1.8 version.

--- src/for_django_1-8/myproject/myproject/urls.py
+++ src/for_django_1-8/myproject/myproject/urls.py
@@ -7,6 +7,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^myapp/',include('myproject.myapp.urls')),
+    url(r'^myapp/', include('myproject.myapp.urls')),
     url(r'^$', RedirectView.as_view(url='/myapp/list/', permanent=True)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
